0b359dd05c07c32d3f1dccede797d68aee1c8775
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-09-23 09:09:28 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,24 +2,62 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  clickedPokemon = () => {
+    this.setState(previousState => {
+      return {
+        clicked: !previousState.clicked
+      }
+    })
+  }
+
   render() {
-    return (
-      <Card>
+    let pokemon = this.props.pokemon
+
+    let cardFront = (
+          <Card onClick={(this.clickedPokemon)} >
+          <div>
+            <div className="image">
+              <img src={pokemon.sprites.front}alt="oh no!" />
+            </div>
+            <div className="content">
+              <div className="header">{pokemon.name}</div>
+            </div>
+            <div className="extra content">
+              <span>
+                <i className="icon heartbeat red" />
+                {pokemon.hp + ' hp'}
+              </span>
+            </div>
+          </div>
+        </Card>
+      )
+
+      let cardBack = (
+        <Card onClick={(this.clickedPokemon)} >
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={pokemon.sprites.back}alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {pokemon.hp + ' hp'}
             </span>
           </div>
         </div>
       </Card>
+    )
+
+    return (
+      !this.state.clicked? cardFront : cardBack
     )
   }
 }
